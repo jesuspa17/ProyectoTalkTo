f4ab2ca57911268bbd92fcfbdbf97ff9d312bcc8
jesuspa17 <jpallares94.jp@gmail.com>
2015-11-20 14:37:09 +0100

    CASI COMPLETO

---
+++ ProyectoAppMessages_V2/app/src/main/java/com/dam/salesianostriana/pmdm/proyectoappmessages/notificaciones/GcmRegistrationAsyncTask.java
@@ -0,0 +1,136 @@
+package com.dam.salesianostriana.pmdm.proyectoappmessages.notificaciones;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Jesús Pallares on 19/11/2015.
+ */
+public class GcmRegistrationAsyncTask extends AsyncTask<String, Void, String> {
+    private GoogleCloudMessaging gcm;
+    private Context context;
+    private String regId;
+    JSONArray response = new JSONArray();
+    String nickName;
+
+    // TODO: change to your own sender ID to Google Developers Console project number, as per instructions above
+    private static final String SENDER_ID = "93663396119";
+
+    public GcmRegistrationAsyncTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        String msg = "";
+        nickName = params[0];
+        try {
+            if (gcm == null) {
+                gcm = GoogleCloudMessaging.getInstance(context);
+            }
+            regId = gcm.register(SENDER_ID);
+            msg = "Device registered, registration ID=" + regId;
+
+            // You should send the registration ID to your server over HTTP,
+            // so it can use GCM/HTTP or CCS to send messages to your app.
+            // The request to your server should be authenticated if your app
+            // is using accounts.
+            sendRegistrationIdToBackend();
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            msg = "Error: " + ex.getMessage();
+        }
+        return msg;
+    }
+
+    @Override
+    protected void onPostExecute(String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+        SharedPreferences prefs = context.getSharedPreferences("preferencias",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("clave",regId);
+        editor.apply();
+
+        Logger.getLogger("REGISTRATION").log(Level.INFO, msg);
+    }
+
+    private void sendRegistrationIdToBackend() {
+        URL url = null;
+        HttpURLConnection urlConnection = null;
+        Log.v("CatalogClient", "Entra en sendRegistration");
+
+        try {
+            url = new URL("http://miguelcr.hol.es/talkme/register?regId=" + regId+"&nickname="+nickName);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            int responseCode = urlConnection.getResponseCode();
+
+            // Si queremos comprobar si el código HTTP = 200 (OK)
+            //if(responseCode == HttpStatusCodes.STATUS_CODE_OK){
+
+            String responseString = readStream(urlConnection.getInputStream());
+            Log.v("CatalogClient", responseString);
+            try {
+                response = new JSONArray(responseString);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            Log.v("CatalogClient", "Error conexión");
+            e.printStackTrace();
+        } finally {
+            urlConnection.disconnect();
+        }
+
+    }
+
+    private String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuffer response = new StringBuffer();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return response.toString();
+    }
+}
---
+++ ProyectoAppMessages_V2/app/src/main/java/com/dam/salesianostriana/pmdm/proyectoappmessages/pojo_listas/ItemUsuario.java
@@ -0,0 +1,21 @@
+package com.dam.salesianostriana.pmdm.proyectoappmessages.pojo_listas;
+
+/**
+ * Created by Jesús Pallares on 19/11/2015.
+ */
+public class ItemUsuario {
+
+    private String nombre;
+
+    public ItemUsuario(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
